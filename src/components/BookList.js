db40bab096589ef2cef2e1e65096882c46afa6b8
saluumaa <salumahassan02@gmail.com>
2023-04-13 15:48:32 +0300

    Add style in the booklist

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,9 +1,41 @@
 import React, { useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { useSelector, useDispatch } from 'react-redux';
+import { CircularProgressbar } from 'react-circular-progressbar';
 import { removeBook, getBooks } from '../redux/books/booksSlice';
+import 'react-circular-progressbar/dist/styles.css';
 
 const BookList = () => {
+  const categoryStyle = {
+    fontSize: '0.875rem',
+    fontFamily: 'Montserrat, sans-serif',
+    color: '#121212',
+    margin: '0 2.438rem 0 0',
+    fontWeight: 'bold',
+  };
+  const titleStyle = {
+    margin: '0.188rem 2.188rem 0 0',
+    fontFamily: 'Roboto Slab, serif',
+    fontSize: '1.375rem',
+    color: '#121212',
+    letterSpacing: '-0.2px',
+  };
+  const authorStyle = {
+    margin: '0.25rem 0 0',
+    fontFamily: 'Roboto Slab, serif',
+    fontSize: '0.875rem',
+    fontWeight: '300',
+    color: '#4386bf',
+  };
+  const chapterNo = {
+    margin: '0.438rem 2.813rem 0.25rem 3.688rem',
+    fontFamily: 'Roboto Slab, serif',
+    fontSize: '1rem',
+    fontWeight: '300',
+    fontStretch: 'normal',
+    letterSpacing: '-0.4px',
+    color: '121212',
+  };
   const { books, isLoading, error } = useSelector((state) => state.books);
   const dispatch = useDispatch();
 
@@ -23,22 +55,51 @@ const BookList = () => {
     <div className="book-wrapper">
       {books.map((book) => (
         <div className="books" key={uuidv4()}>
-          <p className="par category">{book.category}</p>
-          <p className="title">{book.title}</p>
-          <p className="par author">{book.author}</p>
+          <p className="par category" style={categoryStyle}>{book.category}</p>
+          <p className="title" style={titleStyle}>{book.title}</p>
+          <p
+            className="par author"
+            style={authorStyle}
+          >
+            {book.author}
+          </p>
+          <button className="btn comments-btn" type="button">Comments</button>
+          <span className="left-line" />
           <button
             type="button"
             id="button"
-            className="remove-btn"
+            className="btn remove-btn"
             onClick={() => {
               dispatch(removeBook(book.item_id));
             }}
           >
             remove
           </button>
+          <span className="left-line" />
+          <button type="button" className="btn edit-btn">Edit</button>
         </div>
       ))}
 
+      <div className="wrapper">
+        <div className="progress">
+          <CircularProgressbar className="progress-bar" value={70} />
+        </div>
+        <div className="percentage">
+          <h3 style={{ margin: '0.063rem 0 0 0.688rem', fontSize: '2rem' }}>64%</h3>
+          <p style={{
+            margin: '0.438rem 0 0.75rem 0.75rem', opacity: '0.5', fontSize: '0.875rem', color: '121212',
+          }}
+          >
+            Completed
+          </p>
+        </div>
+        <span className="left-line2" />
+        <div className="chapter-info">
+          <h3 className="current-chap">Current Chapter</h3>
+          <h3 style={chapterNo}>Chapter 17</h3>
+          <button type="button" className="update-btn">UPDATE PROGRESS</button>
+        </div>
+      </div>
     </div>
   );
 };
